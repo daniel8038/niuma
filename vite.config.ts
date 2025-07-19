0fd0337f17155ad02c7fffd0f8c92b05be67de89
zzh <zzh@zzhdeMacBook-Pro.local>
2025-07-19 19:21:41 +0800

    plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import image from "@rollup/plugin-image";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(),image()],
 });
